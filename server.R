4fd6156e9bb6ba68e37d2aa4599786df95d6013b
Zhian Kamvar <kamvarz@science.oregonstate.edu>
2015-05-26 10:47:05 -0700

    update message to say "Go!"

--- server.R
+++ server.R
@@ -343,7 +343,7 @@ shinyServer(function(input, output, session) {
     if(!input$submit) {
       plot.new() 
       rect(0, 1, 1, 0.8, col = "indianred2", border = 'transparent' ) + 
-      text(x = 0.5, y = 0.9, "Please select data and click\nthe 'Make My Graph' button.", 
+      text(x = 0.5, y = 0.9, "Please select data and click\nthe 'Go!' button.", 
            cex = 1.6, col = "white")
     } else {
       set.seed(seed())
